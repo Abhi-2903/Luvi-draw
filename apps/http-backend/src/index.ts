8c2362e74026cecebb1f83899c600c9fb8083b7f
Abhimanyu <abhimanyuchachan9@gmail.com>
2025-09-25 01:17:00 +0530

    new progress about zod, common files

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
 import { middleware } from "./middleware";
+//@ts-ignore
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { CreateRoomSchema, CreateUserSchema, SignInSchema } from '@repo/common/types';
 const app = express();
+CreateUserSchema
+
+
 
 app.post("/signin", (req, res)=>{
+
+    const data = SignInSchema.safeParse(req.body);
+    if(!data.success){
+         res.json({
+            msg:"invalid Credentials"
+        })
+        return;
+    }
+
     const userId=1;
     const token=jwt.sign({
          userId
@@ -15,9 +29,28 @@ app.post("/signin", (req, res)=>{
 })
 app.post("/signup", (req, res)=>{
     //zod validation
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+
+return res.json({
+    msg:"invalid data"
+})   
+ }
+
+res.json({
+    userId:"123"
 })
+})
+
 app.post("/room", middleware,(req, res)=>{
     //db call
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+         res.json({
+            msg:"invalid Credentials"
+        })
+        return;
+    }
     
     res.json({
         roomId:123
